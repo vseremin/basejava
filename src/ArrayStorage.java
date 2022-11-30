35f065e4be7416ff3ed13616612092eae2dfa24c
vseremin <eremvyach@gmail.com>
2022-11-30 21:38:39 +0300

    правка ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,52 +5,53 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int lastIndex;
+    private int size;
 
     void clear() {
-        for (int i = 0; i < lastIndex; i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        if (lastIndex < storage.length) {
-            storage[lastIndex] = r;
-            lastIndex++;
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
         } else {
             System.out.println("В массиве нет свободного места");
         }
     }
 
     Resume get(String uuid) {
-        for (Resume resume : storage) {
-            if (resume.uuid.equals(uuid)) {
-                return resume;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
+        System.out.println("Ресюме с " + uuid + " нет в базе!");
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < lastIndex; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                for (int j = i; j < storage.length - 1; j++) {
-                    storage[j] = storage[j + 1];
-                }
-                break;
+                System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+                size--;
+                return;
             }
         }
-        lastIndex--;
+        System.out.println("Ресюме с " + uuid + " нет в базе!");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, lastIndex);
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return lastIndex;
+        return size;
     }
 }
