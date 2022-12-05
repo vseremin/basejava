28fdf9a0664b41c8c0e9719a133c4d5e8c7810a4
vseremin <eremvyach@gmail.com>
2022-12-05 20:32:23 +0300

    корректировка метода clear()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,9 +8,7 @@ public class ArrayStorage {
     private int size;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
