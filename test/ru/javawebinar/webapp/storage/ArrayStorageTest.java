b194f8136bee8cb991c24ea71151338755891ed9
vseremin <113263840+vseremin@users.noreply.github.com>
2022-12-12 19:44:38 +0300

    Update ArrayStorageTest.java
    update

--- test/ru/javawebinar/webapp/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/webapp/storage/ArrayStorageTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertEquals;
 public class ArrayStorageTest extends AbstractArrayStorageTest {
 
     private static final Storage storage = new ArrayStorage();
+    
     public ArrayStorageTest() {
         super(storage);
     }
