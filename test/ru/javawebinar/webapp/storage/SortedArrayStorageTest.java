d00c22a3a394949e36cb53c25d9194ac051de103
vseremin <113263840+vseremin@users.noreply.github.com>
2022-12-12 19:45:03 +0300

    Update SortedArrayStorageTest.java
    update

--- test/ru/javawebinar/webapp/storage/SortedArrayStorageTest.java
+++ test/ru/javawebinar/webapp/storage/SortedArrayStorageTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
     private static final Storage storage = new SortedArrayStorage();
+    
     public SortedArrayStorageTest() {
         super(storage);
     }
