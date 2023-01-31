3f3426c6d85f8dce30684a4afd11e6eef4d5d9a1
vseremin <eremvyach@gmail.com>
2023-01-31 13:34:53 +0300

    Lesson12

--- src/ru/javawebinar/webapp/MainConcurrency.java
+++ src/ru/javawebinar/webapp/MainConcurrency.java
@@ -1,13 +1,23 @@
 package ru.javawebinar.webapp;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
-    private static int counter;
-    private static final Object LOCK = new Object();
-    private static final Object LOCK_2 = new Object();
+//    private static int counter;
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+//    private static final Object LOCK = new Object();
+//    private static final Object LOCK_2 = new Object();
+//    private static final Lock lock = new ReentrantLock();
+    private static final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    private static final Lock WRITE_LOCK = reentrantReadWriteLock.writeLock();
+    private static final Lock READ_LOCK = reentrantReadWriteLock.readLock();
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = ThreadLocal.withInitial(() -> new SimpleDateFormat());
 
 
     public static void main(String[] args) throws InterruptedException {
@@ -26,35 +36,55 @@ public class MainConcurrency {
         System.out.println(thread0.getState());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        CompletionService completionService = new ExecutorCompletionService(executorService);
+
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
+            Future<Integer> future = executorService.submit(() ->
+//            Thread thread = new Thread(() ->
+            {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
+                    System.out.println(threadLocal.get().format(new Date()));
                 }
+                latch.countDown();
+                return 5;
             });
-            thread.start();
-            threads.add(thread);
+
+//            thread.start();
+//            threads.add(thread);
         }
-        threads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        System.out.println(mainConcurrency.counter);
+//        threads.forEach(t -> {
+//            try {
+//                t.join();
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
+        latch.await(1, TimeUnit.SECONDS);
+        executorService.shutdown();
+//        System.out.println(mainConcurrency.counter);
+        System.out.println(mainConcurrency.atomicCounter.get());
         LazySingleton.getInstance();
 
-        implementationDeadlock(LOCK, LOCK_2);
-        implementationDeadlock(LOCK_2, LOCK);
+//        implementationDeadlock(LOCK, LOCK_2);
+//        implementationDeadlock(LOCK_2, LOCK);
     }
 
-    private synchronized void inc() {
+    private void inc() {
 //        synchronized (this) {
 //        synchronized (MainConcurrency.class) {
 //        try {
-            counter++;
+//        WRITE_LOCK.lock();
+//        try {
+//            counter++;
+        atomicCounter.incrementAndGet();
+//        } finally {
+//            WRITE_LOCK.unlock();
+//        }
+
 //                wait();
 //        }
 
